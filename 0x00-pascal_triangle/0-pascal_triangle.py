ceca61342d0d0a97751a546ec10158da015bb2be
EuniceGeorge <georgeunice408.com>
2024-10-09 16:08:59 +0100

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,24 +1,21 @@
+#!/usr/bin/python3
 """
 A pascal triangle that returns the
 list of integers
 """
+
+
 def pascal_triangle(n):
     triangle = [[] for i in range(n)]
 
     for i in range(n):
         for j in range(i+1):
 
-            if (j<i):
-                if (j==0):
+            if (j < i):
+                if (j == 0):
                     triangle[i].append(1)
                 else:
-                    triangle[i].append(triangle[i-1][j]+triangle[i-1][j-1])
-            elif(j==i):
-                    triangle[i].append(1)
+                    triangle[i].append(triangle[i-1][j] + triangle[i-1][j-1])
+            elif (j == i):
+                triangle[i].append(1)
     return triangle
-
-
-
-
-
-
