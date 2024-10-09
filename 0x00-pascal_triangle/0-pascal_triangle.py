128a3538032a49914de5bbf3003e6d17dfa09313
EuniceGeorge <georgeunice408.com>
2024-10-09 15:58:24 +0100

    pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+def pascal_triangle(n):
+    triangle = [[] for i in range(n)]
+
+    for i in range(n):
+        for j in range(i+1):
+
+            if (j<i):
+                if (j==0):
+                    triangle[i].append(1)
+                else:
+                    triangle[i].append(triangle[i-1][j]+triangle[i-1][j-1])
+            elif(j==i):
+                    triangle[i].append(1)
+    return triangle
+
+
+
+
+
+
